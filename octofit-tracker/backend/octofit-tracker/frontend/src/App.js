247e6f6f4b87bfa5ebb4c8996ac52942062c83c3
Tomas Buroci <108458651+TomasBuroci@users.noreply.github.com>
2026-03-19 15:21:29 +0000

    6th commit

--- octofit-tracker/backend/octofit-tracker/frontend/src/App.js
+++ octofit-tracker/backend/octofit-tracker/frontend/src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
 
+import logo from '../public/logo192.png';
+import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link, NavLink } from 'react-router-dom';
 import Activities from './components/Activities';
 import Leaderboard from './components/Leaderboard';
@@ -13,7 +14,10 @@ function App() {
       <div className="container">
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-4 rounded">
           <div className="container-fluid">
-            <Link className="navbar-brand fw-bold" to="/">OctoFit Tracker</Link>
+            <Link className="navbar-brand fw-bold d-flex align-items-center" to="/">
+              <img src={logo} alt="OctoFit Logo" className="App-logo me-2" />
+              OctoFit Tracker
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
